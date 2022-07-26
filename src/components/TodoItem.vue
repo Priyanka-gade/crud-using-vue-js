8a43d1939f221b0afafd4f80e7bc6256e8b7a859
Priyanka-gade <priyanka.gadepg@gmail.com>
2022-07-26 17:54:06 +0530

    todo app completed

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,52 +1,55 @@
 <template>
     <div class="grid justify-items-left m-2 p-2 bg-slate-200">
-        <div >
+        <div>
             <!-- class="line-through" v-bind:class="{'is-complete':items.completed}" -->
             <p>
                 <input type="checkbox" />
                 <!-- v-on:change="markcompleted" v-bind:checked="items.completed" -->
                 {{ items.title }}
-                <button @click="$emit('delete-todo', items.id)" class="del font-serif font-semibold float-right bg-red-500 rounded-md text-white p-1 m-1">Delete</button>
-                <button @click="update" class="font-serif font-semibold float-right bg-green-800 rounded-md text-white p-1 m-1">Edit</button>
+                <button @click="$emit('delete-todo', items.id)"
+                    class="del font-serif font-semibold float-right bg-red-500 rounded-md text-white p-1 m-1">Delete</button>
+                <button @click="update"
+                    class="font-serif font-semibold float-right bg-green-800 rounded-md text-white p-1 m-1">Edit</button>
             </p>
         </div>
         <div v-if="this.flag">
-        <form @submit="updatetodo">
-            <div>
-                <input type="text" v-bind="updateTitle" placeholder="title" required/>
-            </div>
-            <div>
-                <button class="float-left font-serif font-semibold bg-blue-700 rounded-md text-white p-1 m-1">Update</button>
-            </div>
-        </form>
+            <form @submit="updatetodo">
+                <div>
+                    <input type="text" v-model="updateTitle" placeholder="Title" required />
+                </div>
+                <div>
+                    <button
+                        class="float-left font-serif font-semibold bg-blue-700 rounded-md text-white p-1 m-1">Update</button>
+                </div>
+            </form>
         </div>
     </div>
 </template>
 <script>
 export default {
     name: "TodoItem component",
-    props: ['items','Todata'],
-    methods:{
-        update(){
-            this.flag=!this.flag;
+    props: ['items', 'Todata'],
+    methods: {
+        update() {
+            this.flag = !this.flag;
         },
-        updatetodo(e){
+        updatetodo(e) {
             e.preventDefault()
-            this.Todata.filter((items)=>{
-                if(items.id == this.items.id){
+            this.Todata.filter((items) => {
+                if (items.id == this.items.id) {
                     //update the title
-                    this.title = this.updateTitle
+                    items.title = this.updateTitle
                 }
             })
             //reset the flag
             this.flag = false
         }
-        
+
     },
-    data(){
-        return{
-            flag:false,
-            updateTitle:""
+    data() {
+        return {
+            flag: false,
+            updateTitle: ""
         }
     }
 }
